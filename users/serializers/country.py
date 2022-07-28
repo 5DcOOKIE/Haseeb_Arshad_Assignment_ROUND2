6e51d92063a75a9a917e8fc2b7faa47ce03a475a
Haseeb Arshad <haseeb.arshad55@gmail.com>
2022-07-28 23:34:47 +0500

    fixed country seriqlizer to nested

--- users/serializers/country.py
+++ users/serializers/country.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 
 from users.models.country import Country
+from users.models.city import City
+from users.serializers.city import CitySerializer
 
 
 # Country Serializer
 class CountrySerializer(serializers.ModelSerializer):
+    # cities = CitySerializer('cities',
+    #                         many=True, read_only=True)
+    cities = serializers.SerializerMethodField()
+
     class Meta:
         model = Country
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'cities')
+
+
+    def get_cities(self, obj):
+        "obj is a Member instance. Returns list of dicts"""
+        qset = City.objects.filter(country=obj)
+        return [CitySerializer(m).data for m in qset]
